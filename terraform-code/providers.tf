118c2c5c4fbc19d76ee8fdf7b102deb60e3867cf
Andrew K <bluer.mullion.0h@icloud.com>
2026-01-20 19:09:05 +0000

    update

--- terraform-code/providers.tf
+++ terraform-code/providers.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.0"
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 6.2.0"
+      version = "~> 6.0"
     }
   }
 }
